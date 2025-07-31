0c34e132b23dda12f3476c181680bf741f3a6643
Ny <ralaivaonylalaina@gmail.com>
2025-07-31 09:52:07 +0300

    commit

--- main.py
+++ main.py
@@ -2,53 +2,54 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+from datetime import datetime
+from typing import List
 
 app = FastAPI()
 
 
-@app.get("/hello")
-def read_hello(request: Request, is_teacher: bool = None, name: str = "Non défini"):
-    accept_headers = request.headers.get("Accept")
-    if accept_headers != "text/plain":
-        return JSONResponse({"message": "Unsupported Media Type"}, status_code=400)
-    if name == "Non défini" and is_teacher is None:
-        return JSONResponse({"message": "Hello world"}, status_code=200)
-    if is_teacher is None:
-        is_teacher = False
-    if is_teacher:
-        return JSONResponse({"message": f"Hello teacher {name}"}, status_code=200)
-    else:
-        return JSONResponse({"message": f"Hello {name}"}, status_code=200)
+@app.get("/ping")
+async def pong():
+    return {"message": "pong"}
 
 
-class WelcomeRequest(BaseModel):
-    name: str
+@app.get("/home")
+async def home():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Welcome home!</h1>
+    </body>
+    </html>
+    """
 
 
-@app.post("/welcome")
-def welcome_user(request: WelcomeRequest):
-    return {f"Bienvenue {request.name}"}
+posts: List[dict] = []
+class Post(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: datetime
+@app.post("/posts", status_code=201)
+async def create_posts(new_posts: List[Post]):
+    posts.extend([post.dict() for post in new_posts])
+    return posts
 
+@app.get("/posts")
+async def get_posts():
+    return posts
 
-class SecretPayload(BaseModel):
-    secret_code: int
+@app.put("/posts")
+async def update_post(post: Post):
+    post_dict = post.dict()
+    for i, existing_post in enumerate(posts):
+        if existing_post["title"] == post.title:
+            posts[i] = post_dict
+            return posts
 
-
-@app.put("/top-secret")
-def put_top_secret(request: Request, request_body: SecretPayload):
-    auth_header = request.headers.get("Authorization")
-    if auth_header != "my-secret-key":
-        return JSONResponse(
-            status_code=403,
-            content={"error": f"Unauthorized header received: {auth_header}"}
-        )
-
-    secret_code = request_body.secret_code
-    code_length = len(str(secret_code))
-    if code_length != 4:
-        return JSONResponse(
-            status_code=400,
-            content={"error": f"Le code fourni n’est pas à 4 chiffres mais {code_length} chiffres."}
-        )
-
-    return JSONResponse(content={"message": f"Voici le code {secret_code}"}, status_code=200)
+    posts.append(post_dict)
+    return posts
